a1f93eb1ef5ad2c7ca7686c2c7c670756e273a55
Gill Hawkes <gillhawkes1@gmail.com>
2024-07-01 20:38:24 -0400

    added smtp code using nodemailer

--- pages/api/contactme.ts
+++ pages/api/contactme.ts
@@ -5,6 +5,29 @@ import { ContactFormData } from "@/app/interfaces/ContactMe";
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
   const { name, email, message }: ContactFormData = req.body;
   
-  const transporter = nodemailer.createTransport({
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT || '587'),
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+      }
+    });
+
+    const mailParams: Object = {
+      from: process.env.SMTP_USER,
+      to: "contagiousconfidencepodcast@gmail.com",
+      subject: `Website message from ${name}`,
+      text: `You have received a new message from ${name} (${email}: \n\n${message})`
+    };
+
+    await transporter.sendMail(mailParams);
+
+    return res.status(200).json({message: "Message sent."});
+  } catch (error) {
+    console.log('error sending email', error);
+    return res.status(500).json({error: 'error sending email message'});
+  }
+
 }
