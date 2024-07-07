4bdc6468c819fd9c72685125c96880ba29c17eae
gillhawkes1 <gillhawkes1@gmail.com>
2024-07-06 21:40:21 -0400

    updated episode container styling

--- components/podcast/PodcastEpisode.tsx
+++ components/podcast/PodcastEpisode.tsx
@@ -4,7 +4,7 @@ import { Episode } from '@/app/interfaces/Podcast';
 import Image from 'next/image'
 
 const PodcastEpisode: React.FC<Episode> = ({buzzsprout, spotify, apple, podbean}) => {
-  const iconWidthHeight = 50;
+  const iconWidthHeight = 40;
 
   return (
     <main>
@@ -14,28 +14,27 @@ const PodcastEpisode: React.FC<Episode> = ({buzzsprout, spotify, apple, podbean}
           <h3 className="text-lg font-semibold pt-2 text-center">{buzzsprout.title}</h3>
           <h5 className="text-m font-semibold text-center">S{buzzsprout.season_number} E{buzzsprout.episode_number}</h5>
           <div className="episode-links">
-          {buzzsprout && (
-            <a href={buzzsprout.episode_url} target="_blank" rel="noopener noreferrer">
-              <Image src='/assets/buzzsprout.png' alt="Listen on Buzzsprout" className="h-8 w-8 inline-block m-1" width={iconWidthHeight} height={iconWidthHeight} />
-            </a>
-          )}
-          {spotify && (
-            <a href={spotify.external_urls.spotify} target="_blank" rel="noopener noreferrer">
-              <Image src='/assets/spotify.svg' alt="Listen on Spotify" className="h-8 w-8 inline-block m-1" width={iconWidthHeight} height={iconWidthHeight} />
-            </a>
-          )}
-          {apple && (
-            <a href={apple.trackViewUrl} target="_blank" rel="noopener noreferrer">
-              <Image src='/assets/applepodcasts.png' alt="Listen on Apple Podcasts" className="h-8 w-8 inline-block m-1" width={iconWidthHeight} height={iconWidthHeight} />
-            </a>
-          )}
-          {podbean && (
-            <a href={podbean.permalink_url} target="_blank" rel="noopener noreferrer">
-              <Image src='/assets/podbean.png' alt="Listen on Podbean" className="h-8 w-8 inline-block m-1" width={iconWidthHeight} height={iconWidthHeight} />
-            </a>
-          )}
-        </div>
-
+            {buzzsprout && (
+              <a href={buzzsprout.episode_url} target="_blank" rel="noopener noreferrer">
+                <Image src='/assets/buzzsprout.png' alt="Listen on Buzzsprout" className="h-10 w-10 inline-block p-1 m-1" width={iconWidthHeight} height={iconWidthHeight} />
+              </a>
+            )}
+            {spotify && (
+              <a href={spotify.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+                <Image src='/assets/spotify.svg' alt="Listen on Spotify" className="h-10 w-10 inline-block p-1 m-1" width={iconWidthHeight} height={iconWidthHeight} />
+              </a>
+            )}
+            {apple && (
+              <a href={apple.trackViewUrl} target="_blank" rel="noopener noreferrer">
+                <Image src='/assets/applepodcasts.png' alt="Listen on Apple Podcasts" className="h-10 w-10 inline-block p-1 m-1" width={iconWidthHeight} height={iconWidthHeight} />
+              </a>
+            )}
+            {podbean && (
+              <a href={podbean.permalink_url} target="_blank" rel="noopener noreferrer">
+                <Image src='/assets/podbean.png' alt="Listen on Podbean" className="h-10 w-10 inline-block p-1 m-1" width={iconWidthHeight} height={iconWidthHeight} />
+              </a>
+            )}
+          </div>
         </div>
       </div>
     </main>
